682b3fdc0fce728202cfda4ef15785072d5499d0
BackdoorCreatorWorld <backdoorcreator@gmail.com>
2026-02-22 08:21:58 +0700

    Create __init__.py

---
+++ core/utils/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Utils package initialization
+Architect 01 - Professional Suite
+"""
+
+from core.utils import validator
+from core.utils import proxy_manager
+from core.utils import useragents
