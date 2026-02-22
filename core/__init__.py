68ec5d87c943d178953af20e4f5cbb63a96fbee0
BackdoorCreatorWorld <backdoorcreator@gmail.com>
2026-02-22 08:21:29 +0700

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Core package initialization
+Architect 01 - Professional Suite
+"""
+
+__version__ = "2.0.0"
+__author__ = "s3cret_proj3ct"
+__license__ = "MIT"
+
+# Make core a proper package
+from core import handler
+from core import slowloris
+from core import http_flood
+from core import request_spam
+from core import anti_web
+from core import useragent_spam
+from core import firewall_bypass
+from core import proxy_spam
+from core import check_ip
